53b0faef62d603e6521344d5bbe9e4b0fdf51cd2
Francisco Rossi Gabriel <rossigabrielfrancisco2@gmail.com>
2019-05-09 17:51:22 +0200

    "creacio de taula"CMS Fran

--- CMSDB.php
+++ CMSDB.php
@@ -17,10 +17,25 @@ if ($conn->query($sql) === TRUE) {
     $missatge2 = "<span style='color:#4CAF50;'>Status Dbase</span>&nbsp;&xrarr; La Base de dades s'ha creat.";}
   else{
     $missatge2 = "<span style='color:#4CAF50;'>Status Dbase</span>&nbsp;&xrarr; ". $conn->error;
+
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+      $sql = "CREATE TABLE squareclickertb (
+      id INT(100) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      usuari VARCHAR(100) NOT NULL,
+      puntuació INT(200) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      )";
+
+      if ($conn->query($sql) === TRUE) {
+          $missatge3 = "<span style='color:#4CAF50;'>Status Taula</span>&nbsp;&xrarr; La Taula s'ha creat satisfactòriament.";
+      } else {
+          $missatge3 = "<span style='color:#4CAF50;'>Status Taula</span>&nbsp;&xrarr; ". $conn->error;
+    }
   }
 ?>
 <div class="post">
   <p><?php echo $missatge1 ?></p>
   <p><?php echo $missatge2 ?></p>
   <p><?php echo $missatge3 ?></p>
+  <p><?php echo $missatge4 ?></p>
 </div>
